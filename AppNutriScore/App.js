4b6a2d7403e665d63b7f9f9dafb503519dbfdc7a
Emilien Mattei <emilien.mattei1@epsi.fr>
2024-04-12 09:31:37 +0200

    Premier

---
+++ AppNutriScore/App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <TextInput></TextInput>
+      <Button></Button>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
